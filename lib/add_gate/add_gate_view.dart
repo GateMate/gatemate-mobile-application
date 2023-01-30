4cd22752a682f5c51e656a89cac9c385988cc0b5
carissarpatton <crp016@uark.edu>
2023-01-30 00:04:28 -0600

    attempt at mapbox. not 3d functional yet

--- lib/add_gate/add_gate_view.dart
+++ lib/add_gate/add_gate_view.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:mapbox_gl/mapbox_gl.dart';
+
+import '../app_constants.dart';
 
 class AddGateRoute extends StatelessWidget {
   const AddGateRoute({super.key});
@@ -10,12 +14,33 @@ class AddGateRoute extends StatelessWidget {
         title: const Text('Add Gate'),
       ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: const Text('adding a gate'),
+        child: FlutterMap(
+          options: MapOptions(
+            minZoom: 5,
+            maxZoom: 18,
+            zoom: 13,
+            center: AppConstants.myLocation,
+          ),
+          layers: [
+            TileLayerOptions(
+              urlTemplate:
+                  "https://api.mapbox.com/styles/v1/cpatton/cldidgp83000d01mjhyoyw7fg/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiY3BhdHRvbiIsImEiOiJjbGRpODF1ODkwMWFpM3BvaDBhOWRyYWgzIn0.oW6om9jo0s-ZWAWcLiuaRw",
+              // "https://api.mapbox.com/styles/v1/cpatton/cldi9e13q001y01o98005f6wu/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiY3BhdHRvbiIsImEiOiJjbGRpODF1ODkwMWFpM3BvaDBhOWRyYWgzIn0.oW6om9jo0s-ZWAWcLiuaRw",
+              // "https://api.mapbox.com/styles/v1/dhruv25/cldi9e13q001y01o98005f6wu/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiY3BhdHRvbiIsImEiOiJjbGRodG1pZ3kweWFyM3ZvM2trcjY5d3liIn0.Th1u92jVxkdhJp1-pcJpdA",
+              additionalOptions: {
+                // 'mapStyleId': AppConstants.mapBoxStyleId,
+                'id': 'mapbox.terrain-enable',
+                'accessToken': AppConstants.mapBoxAccessToken,
+              },
+            ),
+          ],
         ),
+        // child: ElevatedButton(
+        //   onPressed: () {
+        //     Navigator.pop(context);
+        //   },
+        //   child: const Text('adding a gate'),
+        // ),
       ),
     );
   }
---
+++ lib/app_constants.dart
@@ -0,0 +1,10 @@
+import 'package:latlong2/latlong.dart';
+
+class AppConstants {
+  static const String mapBoxAccessToken =
+      'pk.eyJ1IjoiY3BhdHRvbiIsImEiOiJjbGRpODF1ODkwMWFpM3BvaDBhOWRyYWgzIn0.oW6om9jo0s-ZWAWcLiuaRw';
+
+  static const String mapBoxStyleId = 'cldidgp83000d01mjhyoyw7fg';
+
+  static final myLocation = LatLng(36.0831986, -94.2166087);
+}
