1cb08a743a138066c5261f95f968f772e2ea924c
carissarpatton <crp016@uark.edu>
2023-02-06 22:39:46 -0600

    add long click on markers

--- lib/add_gate/add_gate_view.dart
+++ lib/add_gate/add_gate_view.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -33,19 +31,65 @@ class AddGateRoute extends StatelessWidget {
   Widget build(BuildContext context) {
     _addMarkers();
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Add Gate'),
-        backgroundColor: Colors.green[700],
-      ),
-      body: GoogleMap(
-        onMapCreated: _onMapCreated,
-        mapType: MapType.terrain,
-        markers: _markers,
-        initialCameraPosition: CameraPosition(
-          target: _center,
-          zoom: 10.0,
+        appBar: AppBar(
+          title: const Text('Add Gate'),
+          backgroundColor: Colors.green[700],
         ),
-      ),
-    );
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          mapType: MapType.terrain,
+          markers: _markers,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 10.0,
+          ),
+
+          // Stack(
+          //   children: [
+          //     FlutterMap(
+          //       options: MapOptions(
+          //           minZoom: 5,
+          //           maxZoom: 18,
+          //           zoom: 13,
+          //           center: AppConstants.myLocation),
+          //       layers: [
+          //         TileLayerOptions(
+          //           urlTemplate:
+          //               "https://api.mapbox.com/styles/v1/cpatton/cldievrq0000301o10nbx1zvu/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiY3BhdHRvbiIsImEiOiJjbGRodG1pZ3kweWFyM3ZvM2trcjY5d3liIn0.Th1u92jVxkdhJp1-pcJpdA",
+          //           // "https://api.mapbox.com/styles/v1/dhruv25/{mapStyleId}/tiles/256/{z}/{x}/{y}@2x?access_token={accessToken}",
+          //           additionalOptions: {
+          //             'mapStyleId': 'mapbox.mapbox-outdoors-v11',
+          //             'accessToken': AppConstants.mapBoxAccessToken,
+          //           },
+          //         ),
+          //       ],
+          //     )
+          //   ],
+          // )
+          // child: FlutterMap(
+          //   options: MapOptions(
+          //     minZoom: 5,
+          //     maxZoom: 18,
+          //     zoom: 13,
+          //     center: AppConstants.myLocation,
+          //   ),
+          //   layers: [
+          //     TileLayerOptions(
+          //       urlTemplate:
+          //           "https://api.mapbox.com/styles/v1/cpatton/cldievrq0000301o10nbx1zvu/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiY3BhdHRvbiIsImEiOiJjbGRodG1pZ3kweWFyM3ZvM2trcjY5d3liIn0.Th1u92jVxkdhJp1-pcJpdA",
+          //       additionalOptions: {
+          //         'mapStyleId': AppConstants.mapBoxStyleId,
+          //         'accessToken': AppConstants.mapBoxAccessToken,
+          //       },
+          //     ),
+          //   ],
+          // ),
+          // child: ElevatedButton(
+          //   onPressed: () {
+          //     Navigator.pop(context);
+          //   },
+          //   child: const Text('adding a gate'),
+          // ),
+        ));
   }
 }
