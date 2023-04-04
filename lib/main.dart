911f9e162ac82583d3799e175b9888e9332eb5e9
Jackson Bullard <jsbullar@uark.edu>
2023-04-03 20:41:58 -0500

    Notifications plugin initialization (also temp app icon).

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:gatemate_mobile/model/firebase/gatemate_auth.dart';
 import 'package:gatemate_mobile/model/viewmodels/action_center_view_model.dart';
 import 'package:gatemate_mobile/view/home/home.dart';
@@ -20,6 +21,35 @@ void main() async {
   getIt.registerLazySingleton<ActionCenterViewModel>(
     () => ActionCenterViewModel(),
   );
+
+  // flutter_local_notifications initialization
+  final flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+  // TODO: 'app_icon' needs to be added as a drawable
+  // resource to the Android head project?
+  const initializationSettingsAndroid =
+      AndroidInitializationSettings('gatemate_temp_app_icon');
+  // TODO: This may be necessary for iOS functionality
+  // https://pub.dev/packages/flutter_local_notifications#initialisation
+  // final initializeSettingsDarwin = DarwinInitializationSettings(
+  //   onDidReceiveLocalNotification: ,
+  // )
+  const initializationSettingsLinux = LinuxInitializationSettings(
+    defaultActionName: 'Open notification', // TODO: May want to change?
+  );
+
+  const initializationSettings = InitializationSettings(
+    android: initializationSettingsAndroid,
+    // iOS: initializationSettingsDarwin,   // TODO
+    // macOS: initializationSettingsDarwin,
+    linux: initializationSettingsLinux,
+  );
+
+  await flutterLocalNotificationsPlugin.initialize(
+    initializationSettings,
+    // onDidReceiveBackgroundNotificationResponse: ,
+  );
+
+  // TODO: Request notification permissions (iOS)
 
   // Start app
   runApp(const GateMateApp());
