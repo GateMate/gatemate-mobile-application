b697158fc5917d491ac9743394101de57309dc7c
Carissa Patton <crp016@uark.edu>
2023-04-13 15:55:55 -0500

    merge changes

--- lib/map_pin_pill/map_pin_pill_model.dart
+++ lib/map_pin_pill/map_pin_pill_model.dart
@@ -1,13 +1,13 @@
-import 'dart:ui';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+// import 'dart:ui';
+// import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class PinInformation {
-  LatLng location;
-  String locationName;
-  Color labelColor;
+// class PinInformation {
+//   LatLng location;
+//   String locationName;
+//   Color labelColor;
 
-  PinInformation(
-      {required this.location,
-      required this.locationName,
-      required this.labelColor});
-}
+//   PinInformation(
+//       {required this.location,
+//       required this.locationName,
+//       required this.labelColor});
+// }
