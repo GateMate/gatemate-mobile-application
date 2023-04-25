e08d07974581b90ac6e7847a665d4af3f54b4565
Jackson Bullard <jsbullar@uark.edu>
2023-04-25 12:55:30 -0500

    Added section for manually triggering a weather alert task.

--- lib/view/settings/settings_view.dart
+++ lib/view/settings/settings_view.dart
@@ -4,9 +4,10 @@ import 'package:gatemate_mobile/model/viewmodels/fields_view_model.dart';
 import 'package:gatemate_mobile/view/settings/water_level_row.dart';
 import 'package:gatemate_mobile/view/ui_primatives/horizontal_divider.dart';
 import 'package:get_it/get_it.dart';
-import 'package:provider/provider.dart';
+import 'package:workmanager/workmanager.dart';
 
 import '../ui_primatives/confirmation_button.dart';
+import '../ui_primatives/custom_input_field.dart';
 import 'field_selection_row.dart';
 import 'notification_settings_row.dart';
 
@@ -45,9 +46,80 @@ class SettingsView extends StatelessWidget {
               },
               buttonText: 'Sign Out',
             ),
+            const HorizontalDivider(),
+            const Text(
+              'Developer Tools (DEBUGGING ONLY)',
+              style: TextStyle(fontSize: 18),
+            ),
+            const SizedBox(height: 15),
+            TriggerDebugRow(),
           ],
         ),
       ),
+    );
+  }
+}
+
+class TriggerDebugRow extends StatefulWidget {
+  const TriggerDebugRow({
+    super.key,
+  });
+
+  @override
+  State<TriggerDebugRow> createState() => _TriggerDebugRowState();
+}
+
+class _TriggerDebugRowState extends State<TriggerDebugRow> {
+  final _latController = TextEditingController();
+  final _longController = TextEditingController();
+
+  @override
+  void dispose() {
+    _latController.dispose();
+    _longController.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            Expanded(
+              child: CustomInputField(
+                inputController: _latController,
+                hintText: 'Latitude',
+              ),
+            ),
+            const SizedBox(width: 8),
+            Expanded(
+              child: CustomInputField(
+                inputController: _longController,
+                hintText: 'Longitude',
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(height: 15),
+        ElevatedButton(
+          onPressed: () {
+            Workmanager().registerOneOffTask(
+              'DEBUG',
+              'weather-fetcher',
+              inputData: {
+                'latitude': double.parse(_latController.text),
+                'longitude': double.parse(_longController.text),
+              },
+            );
+          },
+          child: const Padding(
+            padding: EdgeInsets.symmetric(vertical: 12),
+            child: Text('Weather Alert Debug'),
+          ),
+        ),
+      ],
     );
   }
 }
